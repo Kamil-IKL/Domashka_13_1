5600a44805d2b380dae3af4c6c986cb835d46bff
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-08-16 20:50:24 +0500

    Add files via upload

---
+++ 13_ikl_M13_DZ_46.py
@@ -0,0 +1,26 @@
+import time
+import asyncio
+
+
+class Tournament():
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+        quantity_
+
+
+
+def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    time.sleep(5)
+    print(f'Силач {name} поднял <номер шара>')
+    print(f'Силач {name} закончил соревнования.')
+
+
+def counter_ball():
+    a = 0
+    quantity_ball = 5
+    while True:
+        time.sleep(2)
+        a += 1
+        quantity_ball -=
