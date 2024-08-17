f92731f8d3cfb56e626d89579980ae18518f2eab
Kamil-IKL <161152970+Kamil-IKL@users.noreply.github.com>
2024-08-17 12:59:51 +0500

    Update 13_ikl_M13_DZ_46.py

--- 13_ikl_M13_DZ_46.py
+++ 13_ikl_M13_DZ_46.py
@@ -1,26 +1,37 @@
 import time
 import asyncio
 
-
-class Tournament():
-    def __init__(self, name, power):
-        self.name = name
-        self.power = power
-        quantity_
+"""
+cначала написал код (файл "probe") потом здесь ассинхронизировал
+"""
 
 
-
-def start_strongman(name, power):
+async def start_strongman(name: str, power: int):
+    quantity_ball = 5
     print(f'Силач {name} начал соревнования.')
-    time.sleep(5)
-    print(f'Силач {name} поднял <номер шара>')
+    i = -1
+    while i < quantity_ball - 1:
+        i += 1
+        # time.sleep(1 / power)
+        await asyncio.sleep(1 / power)
+        print(f'Силач {name} поднял {quantity_ball - i} шар')
     print(f'Силач {name} закончил соревнования.')
 
 
-def counter_ball():
-    a = 0
-    quantity_ball = 5
-    while True:
-        time.sleep(2)
-        a += 1
-        quantity_ball -=
+async def start_tournament():
+    print('Start')
+    task_1 = asyncio.create_task(start_strongman('Василий', 3))
+    task_2 = asyncio.create_task(start_strongman('Явдат', 1))
+    task_3 = asyncio.create_task(start_strongman('Силантий', 2))
+    await task_1
+    await task_2
+    await task_3
+    print('Finish')
+
+
+
+start = time.time()
+asyncio.run(start_tournament())
+finish = time.time()
+
+print(f'\nWorking time = {finish - start} second')
